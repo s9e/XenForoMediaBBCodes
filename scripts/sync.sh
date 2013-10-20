823908a2270d5addb46f7cdd69cf571beda44b38
s9e <s9e.dev@gmail.com>
2013-10-20 08:10:08 +0200

    Updated script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+cd $(dirname $(dirname $(realpath $0)))
 git submodule update --remote
-$(dirname $(realpath $0))/build.php
+scripts/build.php
 git diff
 git commit -a && git push
+cp www/configure.html ../s9e.github.io/XenForoMediaBBCodes/
+cd ../s9e.github.io/XenForoMediaBBCodes/
+git commit -a -m"Updated XenForoMediaBBCodes" && git push
