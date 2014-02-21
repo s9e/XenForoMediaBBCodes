05a403f5bdc3b0a84167e6ee9550e133b7890672
s9e <s9e.dev@gmail.com>
2014-02-21 23:33:11 +0100

    Updated sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,7 +3,7 @@ cd $(dirname $(dirname $(realpath $0)))
 git submodule update --remote
 phpunit || exit
 rm -f releases/*-dev*
-scripts/build.php "$@"
+php scripts/build.php "$@"
 git diff
 git commit -a                                               && \
 git push                                                    && \
