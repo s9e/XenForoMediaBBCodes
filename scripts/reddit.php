223e56fc6bff1ac47df4a18cc39543b1c0e2df98
s9e <s9e.dev@gmail.com>
2014-07-29 19:54:10 +0200

    Updated helper script [ci skip]

--- scripts/reddit.php
+++ scripts/reddit.php
@@ -1,7 +1,18 @@
 #!/usr/bin/php
 <?php
 
-$url = 'http://www.reddit.com/domain/' . $_SERVER['argv'][1] . '/new/.json?limit=35';
+$url = 'http://www.reddit.com/domain/' . $_SERVER['argv'][1] . '/';
+
+if (isset($_SERVER['argv'][2]))
+{
+	$url .= 'search.json?q=url%3Avideos&sort=new&restrict_sr=on&';
+}
+else
+{
+	$url .= 'new/.json?';
+}
+
+$url .= 'limit=35';
 
 $response = json_decode(file_get_contents($url), true);
 foreach ($response['data']['children'] as $child)
