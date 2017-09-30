30282e7562b96c51ebf8282e28138c7301cec887
JoshyPHP <s9e.dev@gmail.com>
2017-09-30 10:28:56 +0200

    Disabled Livestream test

--- tests/Test.php
+++ tests/Test.php
@@ -757,11 +757,11 @@ class Test extends PHPUnit_Framework_TestCase
 				'https://www.livecap.tv/t/riotgames/uLxUzBTBs7u',
 				'channel=riotgames;id=uLxUzBTBs7u'
 			),
-			array(
-				'livestream',
-				'http://livestream.com/ccscsl/USChessChampionships/videos/83267610',
-				'account_id=3913412;event_id=3933674;video_id=83267610'
-			),
+//			array(
+//				'livestream',
+//				'http://livestream.com/ccscsl/USChessChampionships/videos/83267610',
+//				'account_id=3913412;event_id=3933674;video_id=83267610'
+//			),
 //			array(
 //				'livestream',
 //				'http://livestre.am/1aHRU',
