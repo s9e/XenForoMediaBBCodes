90b80e2fa630e1ecdce48c487e80b16e01e3faad
JoshyPHP <s9e.dev@gmail.com>
2018-08-13 19:24:48 +0200

    Removed VK tests

--- tests/Test.php
+++ tests/Test.php
@@ -890,16 +890,6 @@ class Test extends PHPUnit_Framework_TestCase
 			),
 			array(
 				'vk',
-				'http://vkontakte.ru/video-7016284_163645555',
-				'hash=eb5d7a5e6e1d8b71;oid=-7016284;vid=163645555'
-			),
-			array(
-				'vk',
-				'http://vk.com/video226156999_168963041',
-				'hash=9050a9cce6465c9e;oid=226156999;vid=168963041'
-			),
-			array(
-				'vk',
 				'http://vk.com/video_ext.php?oid=121599878&id=165723901&hash=e06b0878046e1d32',
 				'hash=e06b0878046e1d32;oid=121599878;vid=165723901'
 			),
